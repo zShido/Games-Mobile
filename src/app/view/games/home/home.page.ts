bbb5e4574f7b365fd506557ed57abd07f8326d10
IsraelHF <israel.com.ihf80@gmail.com>
2024-03-13 21:59:18 -0300

    Mudanças exibição das listas de jogos(completa e filtrada)

--- src/app/view/games/home/home.page.ts
+++ src/app/view/games/home/home.page.ts
@@ -10,44 +10,46 @@ import { IonSearchbar } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  private originalListaJogos: Jogo[] = [];
-  public lista_jogos: Jogo[] = [];
-  public Genero = Genero;
-  public user: any;
+  lista_jogos: Jogo[] = [];
+  jogos: Jogo[] = [];
   isLoading: boolean = false;
   hasSearched: boolean = false;
   query: string = '';
   @ViewChild('mySearchbar', { static: false })
   searchbar!: IonSearchbar;
+  public user: any;
+  emptySearchModel: any = {
+    icon: 'search-outline',
+    title: 'Nenhum jogo encontrado.',
+  };
+  model: any = {
+    icon: 'game-controller-outline',
+    title: 'nenhum jogo cadastrado',
+  };
 
   constructor(
-    private router: Router,
-    private firebaseService: FirebaseService,
-    private authService: AuthService
+    private authService: AuthService,
+    private firebase: FirebaseService,
+    private router: Router
   ) {
     this.user = this.authService.getUserLogged();
     console.log(this.user);
-    this.carregarListaJogos();
-  }
-
-  carregarListaJogos() {
     this.isLoading = true;
     setTimeout(() => {
-      this.firebaseService.read(this.user.uid).subscribe((res) => {
-        this.originalListaJogos = res.map((jogo) => {
+      this.firebase.read(this.user.uid).subscribe((res) => {
+        this.lista_jogos = res.map((jogos) => {
           return {
-            id: jogo.payload.doc.id,
-            ...(jogo.payload.doc.data() as any),
+            id: jogos.payload.doc.id,
+            ...(jogos.payload.doc.data() as any),
           } as Jogo;
         });
-        this.lista_jogos = [...this.originalListaJogos]; // Copia da lista original
-        this.isLoading = false;
       });
+      this.isLoading = false;
     }, 2000);
   }
 
   irParaCadastrar() {
-    this.router.navigate(['/cadastrar']);
+    this.router.navigate(['/cadastro']);
   }
 
   editarJogo(jogo: Jogo) {
@@ -65,26 +67,32 @@ export class HomePage {
   }
 
   async onSearchChange(event: any) {
-    this.isLoading = true;
+    this.hasSearched = true;
     this.query = event.detail.value.toLowerCase();
+    this.jogos = [];
     if (this.query.length > 0) {
+      this.isLoading = true;
       setTimeout(() => {
         // Filtrar a lista original de jogos com base no termo de pesquisa
-        const filteredJogos = this.originalListaJogos.filter((jogo) =>
-          jogo.nome.toLowerCase().includes(this.query)
-        );
-        // Atualizar a lista de jogos com os resultados da pesquisa
-        this.lista_jogos = filteredJogos;
-        // Se a pesquisa retornar resultados, atualizar o estado hasSearched para true
-        this.hasSearched = true;
-        console.log(this.lista_jogos);
+        this.jogos = this.lista_jogos.filter((jogo: any) => {
+          return jogo.nome.toLowerCase().includes(this.query);
+        });
+        console.log(this.jogos);
         this.isLoading = false;
       }, 2000);
     } else {
-      // Se a pesquisa estiver vazia, redefinir o estado hasSearched para false
-      // e recarregar a lista completa de jogos
-      this.hasSearched = false;
-      this.carregarListaJogos();
+      // Se a pesquisa estiver vazia, redefinir a lista de jogos para a lista original
+      this.jogos = [...this.lista_jogos];
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2000);
     }
+  }
+
+  returnSearch() {
+    this.hasSearched = false;
+    this.jogos = [...this.lista_jogos];
+    this.searchbar.value = null;
   }
 }
